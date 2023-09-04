3bd7867e9a819c967e6a0cad719c77def5d70218
Aleksander Osikowicz <tupolewcx@gmail.com>
2023-09-04 17:47:56 +0200

    Reverting old models

--- goamapper/models.py
+++ goamapper/models.py
@@ -17,14 +17,13 @@ class Template(BaseModel):
     width: int
     height: int
     background_fill: str
-    map_frame: MapFrame = None
+    map_frame: MapFrame | None
     bottom_area_height: int
-    text_boxes: list[TextBox] = None
+    text_boxes: list[TextBox] | None
 
 class Poster(BaseModel):
-    bbox: list[float] = None
-    place_name: str
-    radius: int
+    bbox: list[float]
+    dir_name: str
     poster_name: str
     template: Template
     map_layers: dict
